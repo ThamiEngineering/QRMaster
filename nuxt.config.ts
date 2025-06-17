ef387ef9b80bd5f2d45ed1ed5f01d4cbeceb15bf
Thami <developer@thami.fr>
2025-06-17 10:43:12 +0200

    🎉 Initial commit - QRMaster project setup with Nuxt 3, TypeScript, Tailwind, Pinia

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@pinia/nuxt'
+  ],
+  typescript: {
+    strict: true,
+    typeCheck: false,
+  }
+})
